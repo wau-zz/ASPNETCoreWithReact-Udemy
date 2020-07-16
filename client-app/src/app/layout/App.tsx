0da1d1a4b2b4e5ac3f12299b967e0eed0209f314
wau-zz <will.au@gmail.com>
2020-07-16 00:31:07 -0700

    Section 5 sub-section 61.

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import {Header, Icon, List, Container } from 'semantic-ui-react';
+
+import axios from 'axios';
+import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+interface IState {
+  activities: IActivity[]
+}
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0])
+
+  }
+
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      });
+       
+
+  }, []);
+  /*
+  componentDidMount() {
+
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        //console.log(response);
+        this.setState({
+          activities: response.data
+        })
+      })
+  }
+*/
+
+  
+    return (
+      <Fragment>
+        <NavBar />
+        <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard activities={activities} selectActivity={handleSelectActivity}  selectedActivity={selectedActivity!} />
+          
+        </Container>
+       </Fragment>
+      
+    );
+
+  
+}
+
+export default App;
---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Image, Card, Icon, Button } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+
+interface IProps {
+
+    activity: IActivity;
+}
+
+const ActivityDetails: React.FC<IProps> = ({activity}) => {
+    return (
+        <Card fluid>
+            <Image src={'/assets/categoryImages/${activity.category}.jpg'} wrapped ui={false} />
+            <Card.Content>
+            <Card.Header>{activity.title}</Card.Header>
+            <Card.Meta>
+                <span>{activity.date}</span>
+            </Card.Meta>
+            <Card.Description>
+                {activity.description}
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group widths='10'>
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
+
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default ActivityDetails;
